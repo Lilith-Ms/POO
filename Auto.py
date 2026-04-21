4e4bcbec89d6f4f2eb79bb2aecec5becd76e2692
Lilith-Ms <jaz.chik.283@gmail.com>
2026-04-21 16:16:15 -0500

    Feat: Métodos Añadidos a Los ejercicios en Clase

---
+++ Auto.py
@@ -0,0 +1,68 @@
+class Auto: 
+    def __init__(self, marca, color, ruedas):
+        self.marca = marca
+        self.color = color 
+        self.ruedas = ruedas
+
+ # Métodos añadidos
+    def mostrar_info(self):
+        print(f"marca: {self.marca}")
+        print(f"color: {self.color}")
+        print(f"ruedas: {self.ruedas}")
+
+    def encender(self):
+        print(f"El auto {self.marca} está encendido")
+
+
+#Probar lo que hicimos
+mi_auto = Auto ("Toyota","Rojo", 4)
+print (f"marca: {mi_auto.marca}")
+print (f"color: {mi_auto.color}")
+print (f"ruedas: {mi_auto.ruedas}")
+
+# Uso de métodos
+mi_auto.mostrar_info()
+mi_auto.encender()
+
+auto2 = Auto("Honda","Lila",4)
+
+auto3 = Auto("Ford","Verde",4)
+
+print (f"Marca: { auto2.marca}")
+
+
+#Ejercicio 1
+class Celular:
+    def __init__(self,diseño,batería,SistemaOperativo):
+        self.diseño = diseño
+        self.batería = batería
+        self.SistemaOperativo = SistemaOperativo 
+
+# Métodos añadidos
+    def mostrar_info(self):
+        print(f"diseño: {self.diseño}")
+        print(f"batería: {self.batería}")
+        print(f"SistemaOperativo: {self.SistemaOperativo}")
+
+    def encender(self):
+        print("El celular está encendido")
+
+    def cargar(self):
+        print("El celular se está cargando")
+
+#Prueba
+mi_Celular = Celular ("Ergonómico","3000Mhz","Android 16")
+print (f"diseño: {mi_Celular.diseño}")
+print (f"batería: {mi_Celular.batería}")
+print (f"SistemaOperativo: {mi_Celular.SistemaOperativo}")
+
+# Uso de métodos 
+mi_Celular.mostrar_info()
+mi_Celular.encender()
+mi_Celular.cargar()
+
+celular2 = Celular ("A Prueba de Agua","4000Mhz","Android 13")
+celular3 = Celular ( "Z Flip","5000Mhz","Android 15")
+
+print (f"SistemaOperativo: { celular3.SistemaOperativo}")
+
